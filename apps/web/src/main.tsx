f001913b86e5cd5bae249f0a0a0346959d1a59ab
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2024-03-22 11:18:12 +0530

    feat(web): react-router setup

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { browserRouter } from './router'
+import {RouterProvider} from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={browserRouter} />
   </React.StrictMode>,
 )
