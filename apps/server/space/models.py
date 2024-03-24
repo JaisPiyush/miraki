ccb9b746750d46404b14c6153021571f48fc87a6
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2024-03-24 19:26:27 +0530

    added django server

---
+++ apps/server/space/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import models as auth_models
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        auth_models.User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    avatar = models.URLField()
+
+
