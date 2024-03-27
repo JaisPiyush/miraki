ff1c0b8f5fdaea446906035d36ddcf5d883ae0b3
sandeshmurdia <sandeshmurdia123@gmail.com>
2024-03-27 19:07:49 +0530

    changes for css

--- packages/miraki-trello/src/App.tsx
+++ packages/miraki-trello/src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from '@/component/Header';
 import Board from '@/component/Board';
+import { ThemeProvider } from './components/theme-provider';
+import { ModeToggle } from './components/mood-toggle';
 
 function App() {
 
   return (
-    <main >
-      <Header />
-      <Board /> 
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div style={{paddingLeft: "10%", paddingRight: "10%", paddingBottom: "150px", paddingTop: '50px'  }}>
+       <ModeToggle/>
+       <Header />
+       <Board /> 
       {/* Add other components or content here */}
-    </main>
+      </div>
+      </ThemeProvider>
   );
 }
 
---
+++ packages/miraki-trello/src/components/mood-toggle.tsx
@@ -0,0 +1,25 @@
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "@/components/theme-provider"
+import { Switch } from "@/components/ui/switch"
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
+  return (
+    <div style={{display: 'flex', position: 'absolute', right: '0px', top: '20px', width: '100px'}}>
+      <Switch id="airplane-mode" onClick={toggleTheme} style={{marginRight: '10px'}} />
+      {theme === 'light' ? (
+        <>
+          <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+        </>
+      ) : (
+        <>
+          <Moon className="h-[1.2rem] w-[1.2rem] transition-all"/>
+        </>
+      )}
+    </div>
+  )
+}
