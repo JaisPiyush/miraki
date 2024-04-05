4806349883c06997611f6f10a0e1dae64427f50f
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2024-04-06 02:17:18 +0530

    fix: vite env

--- apps/web/src/lib/api/base.ts
+++ apps/web/src/lib/api/base.ts
@@ -12,7 +12,7 @@ export const hasAuthenticationToken = () => {
 }
 
 export const api = axios.create({
-    baseURL: import.meta.env['API_BASE_URL'] || 'http://localhost:8000/api/',
+    baseURL: import.meta.env['VITE_API_BASE_URL'] || 'http://localhost:8000/api/',
     headers: {
         Authorization: getAuthenticationToken()
     }
